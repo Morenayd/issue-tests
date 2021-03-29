f6f8e72039a9af22052a6561b27ce05a2acc3347
Folashade Daniel <beccadaniels1824@gmail.com>
2021-03-29 09:33:55 +0100

    modify test js file to get file path from command line and add script to run mocha test

--- issue2/html_task.js
+++ issue2/html_task.js
@@ -1,15 +1,21 @@
+var file_path = process.env.FILE_PATH;
 var webdriver = require('selenium-webdriver');
 var assert = require('chai').assert;
 var { Builder, By } = require("selenium-webdriver");
 
 describe("Testing Scholars Project", function () {
-    this.timeout(5 * 1000 * 60);
+    this.timeout(3 * 1000 * 60);
     var driver = new webdriver.Builder()
         .forBrowser('chrome')
         .build();
 
     before(function setupWebdriver(done) {
-        driver.get("file:///C:/Users/oemma/mocha_test/Dufuna-Fem/submissions/desolaOluseye/HTML/productPage.html").then(done)
+        var file_url = `file://${file_path}`;
+        driver.get(file_url).then(done);
+    });
+
+    after(function() {
+        driver.quit();
     });
 
     it("test case: check if Price is present", async () => {
